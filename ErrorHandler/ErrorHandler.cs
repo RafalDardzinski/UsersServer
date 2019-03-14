455b30be12bfb2e04e8199f608ca2c3221ad20b1
Rafał <rafal.dardzinski@gmail.com>
2019-03-14 01:47:59 +0100

    Added: ErrorHandler for handling exceptions from single place

---
+++ ErrorHandler/ErrorHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersServer.ErrorHandler
+{
+    class ErrorHandler
+    // klasa obsługująca wyjątki na podstawie typów.
+    {
+        public static void Handle(Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
