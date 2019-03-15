bb6cfc13030fb8aeab114ae8ba15154b8cd52af1
Rafał <rafal.dardzinski@gmail.com>
2019-03-15 02:42:38 +0100

    added: Repository abstract class, UsersRepository draft

---
+++ Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersServer.Database;
+
+namespace UsersServer
+{
+    public abstract class Repository
+    {
+        public delegate void RepositoryCommand(NHibernate.ISession session);
+        protected readonly IDatabaseManager _databaseManager;
+        protected Repository(IDatabaseManager databaseManager)
+        {
+            _databaseManager = databaseManager;
+        }
+    }
+}
---
+++ User/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using UsersServer.Database;
+
+namespace UsersServer.User
+{
+    public class UserRepository : Repository, IRepository
+    {
+        public UserRepository(IDatabaseManager databaseManager) : base(databaseManager)
+        {
+        }
+
+
+        private void AddUser(NHibernate.ISession session)
+        {
+            Console.WriteLine("User created yo!");
+        }
+
+        public void Create(Model model)
+        {
+            RepositoryCommand cmd = AddUser;
+            _databaseManager.Execute(cmd);
+        }
+
+        public List<Model> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model Read(Delegate filterFunc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
