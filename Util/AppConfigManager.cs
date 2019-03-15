b1c9daf5c2ca0fc993333dddec0095778c3af8c0
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-03-15 17:46:43 +0100

    Added: AppConfigManager to manage application config.

---
+++ Util/AppConfigManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersServer.Util
+{
+    public class AppConfigManager
+    {
+        public static void SetConnectionString(string connectionString)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var connectionStringsSection = (ConnectionStringsSection)config.GetSection("connectionStrings");
+            connectionStringsSection.ConnectionStrings["AppDatabase"].ConnectionString = connectionString;
+            config.Save();
+        }
+
+        public static string GetConnectionString() => ConfigurationManager.ConnectionStrings["AppDatabase"].ConnectionString;
+    }
+}
