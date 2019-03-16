fc939ddb7f57a1f00695a3ca7b49bebcfda9b8f9
Rafał <rafal.dardzinski@gmail.com>
2019-03-16 18:55:18 +0100

    Added: Logger, moved AppConfigManager to the root.

---
+++ AppConfigManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersServer.Util
+{
+    // Pozwala na zarządzanie konfiguracją aplikacji.
+    public class AppConfigManager
+    {
+        // Zapisuje connection string aby nie było potrzebne precyzowanie go podczas operacji na danych.
+        public static void SetConnectionString(string connectionString)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var connectionStringsSection = (ConnectionStringsSection)config.GetSection("connectionStrings");
+            connectionStringsSection.ConnectionStrings["AppDatabase"].ConnectionString = connectionString;
+            config.Save();
+        }
+
+        // Pobiera connection string.
+        public static string GetConnectionString() => ConfigurationManager.ConnectionStrings["AppDatabase"].ConnectionString;
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersServer
+{
+    // Pozwala na logowanie wydarzeń. Obecnie po prostu wypisuje wiadomości na konsoli, ale można go w przyszłości rozszerzyć o dodatkowe funkcjonalności.
+    public class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
