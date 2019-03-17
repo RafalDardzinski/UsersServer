cae66ff5dc41f05123dfbbc3731aa06977239d85
Rafał <rafal.dardzinski@gmail.com>
2019-03-17 01:26:37 +0100

    Added: User service, refactored UserRepository, implemented Read method.

---
+++ User/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersServer.Database;
+
+namespace UsersServer.User
+{
+    public class User
+    {
+        private static UserRepository repository = new UserRepository(new MsSqlDatabaseManager(AppConfigManager.GetConnectionString()));
+        public static void Create(string firstname, string lastname, string username, string password)
+        {
+            var user = new UserModel
+            {
+                FirstName = firstname,
+                LastName = lastname,
+                Username = username,
+                Password = password
+            };
+            repository.Create(user);
+        }
+
+        public static IList<UserModel> Read(int id, string firstname, string lastname, string username)
+        {
+            return repository.Read(id, firstname, lastname, username);
+            
+        }
+    }
+
+}
--- User/UserRepository.cs
+++ User/UserRepository.cs
@@ -4,37 +4,61 @@ using System.Linq;
 using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
+using NHibernate.Criterion;
 using UsersServer.Database;
 
 namespace UsersServer.User
 {
-    public class UserRepository : Repository, IRepository
+    // Repozytorium użytkowników odpowiada za bezpośrednie operacje na bazie danych.
+    // Każdej publicznej metodzie odpowiada także metoda prywatna która przekazywana jest do MsSqlDatabaseManager jako delegat a następnie wywoływana z instancją sesji jako argumentem.
+    // Dzięki temu logika zapytań pozostaje w repozytorium, a zarządzaniem sesją i tranzakcjami zajmuje się DatabaseManager.
+    public class UserRepository : Repository
     {
+        private UserModel _userToAdd;
+        private SearchCriteria _searchCriteria;
+        private IList<UserModel> _foundUsers;
+
         public UserRepository(IDatabaseManager databaseManager) : base(databaseManager)
         {
         }
 
-
-        private void AddUser(NHibernate.ISession session)
-        {
-            Console.WriteLine("User created yo!");
-        }
-
         public void Create(Model model)
         {
-            RepositoryCommand cmd = AddUser;
-            _databaseManager.Execute(cmd);
+            _userToAdd = (UserModel)model;
+            RepositoryCommand cmd = _Create;
+            DatabaseManager.Execute(cmd);
+            Logger.Log("User created.");
         }
 
-        public List<Model> Read()
+        private void _Create(NHibernate.ISession session)
         {
-            throw new NotImplementedException();
+            session.Save(_userToAdd);
         }
 
-        public Model Read(Delegate filterFunc)
+        public IList<UserModel> Read(int id, string firstname, string lastname, string username)
         {
-            throw new NotImplementedException();
+            _searchCriteria = new SearchCriteria(id, firstname, lastname, username);
+            RepositoryCommand cmd = _Read;
+            DatabaseManager.Execute(cmd);
+            return _foundUsers;
         }
+
+        private void _Read(NHibernate.ISession session)
+        {
+            var c = _searchCriteria;
+            var query = session.QueryOver<UserModel>();
+            if (c.Id != 0)
+                query.Where(u => u.Id == c.Id);
+            if (!String.IsNullOrEmpty(c.FirstName))
+                query.Where(u => u.FirstName == c.FirstName);
+            if (!String.IsNullOrEmpty(c.LastName))
+                query.Where(u => u.LastName == c.LastName);
+            if (!String.IsNullOrEmpty(c.Username))
+                query.Where(u => u.Username == c.Username);
+
+            _foundUsers = query.List();
+        }
+
 
         public void Update()
         {
@@ -44,6 +68,22 @@ namespace UsersServer.User
         public void Delete()
         {
             throw new NotImplementedException();
+        }
+    }
+
+    class SearchCriteria
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+
+        public SearchCriteria(int id, string firstname, string lastname, string username)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+            Username = username;
         }
     }
 }
