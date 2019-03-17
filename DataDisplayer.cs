3b5f7d584bbedb9de9748e557cd1cf9d5f9d9769
Rafał <rafal.dardzinski@gmail.com>
2019-03-17 01:19:04 +0100

    added: DataDisplayer for logging fetched data on console

---
+++ DataDisplayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersServer.Group;
+using UsersServer.User;
+
+namespace UsersServer
+{
+    // Klasa "konsumująca" dane i wyświetlająca je na ekranie.
+    class DataDisplayer
+    {
+        public static void Display(IList<UserModel> users)
+        {
+            string displayText = "";
+            foreach (var user in users)
+            {
+                displayText += $"Id: {user.Id}\nUsername: {user.Username}\nFirstName: {user.FirstName}\nLastName: {user.LastName}\n\n";
+            }
+            Logger.Log(displayText);
+        }
+    }
+}
