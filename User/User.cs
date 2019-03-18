58bb8d88f4dbc3a6197ca72564edcda9a822200d
Rafał <rafal.dardzinski@gmail.com>
2019-03-18 13:54:01 +0100

    Refactored User group operations to work with refactored Repository class

--- User/User.cs
+++ User/User.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NHibernate;
 using UsersServer.Database;
 
 namespace UsersServer.User
 {
     public class User
     {
-        private static UserRepository repository = new UserRepository(new MsSqlDatabaseManager(AppConfigManager.GetConnectionString()));
+        private static readonly UserRepository Repository = new UserRepository(new MsSqlDatabaseManager(AppConfigManager.GetConnectionString()));
 
         public static void Create(string firstname, string lastname, string username, string password)
         {
@@ -20,26 +21,96 @@ namespace UsersServer.User
                 Username = username,
                 Password = password
             };
-            repository.Create(user);
+            Repository.Create(user);
         }
 
         public static IList<UserModel> Read(int id = 0, string firstname = null, string lastname = null, string username = null)
         {
-            return repository.Read(id, firstname, lastname, username);
+            var searchProperties = new Dictionary<string, string>
+            {
+                {"Id", id.ToString()},
+                {"FirstName", firstname },
+                {"LastName", lastname },
+                {"Username", username }
+
+            };
+
+            var searchCriteria = new UserSearchCriteria(searchProperties);
+            return Repository.Read(searchCriteria);
         }
 
         public static void Update(int id, string newFirstName = null, string newLastName = null, string newUsername = null, string newPassword = null)
         {
             var user = Read(id).FirstOrDefault();
-            repository.Update(user, newFirstName, newLastName, newUsername, newPassword);
+            if (user == null)
+                throw new InvalidOperationException("User not found.");
+
+            var newProperties = new Dictionary<string, dynamic>
+            {
+                {"FirstName", newFirstName},
+                {"LastName", newLastName},
+                {"Username", newUsername},
+                {"Password", newPassword}
+            };
+
+            var updatedProperties = new UserUpdatedProperties(newProperties);
+
+            Repository.Update(user, updatedProperties);
         }
 
         public static void Delete(int id)
         {
             var user = Read(id).FirstOrDefault();
-            repository.Delete(user);
+            if (user == null)
+                throw new InvalidOperationException("User not found.");
+            Repository.Delete(user);
         }
 
+        internal class UserSearchCriteria : SearchCriteria<UserModel>
+        {
+            public UserSearchCriteria(Dictionary<string, string> filterProperties) : base(filterProperties)
+            {
+            }
+
+            public override void ApplyToQuery(IQueryOver<UserModel, UserModel> query)
+            {
+                _filterProperties.TryGetValue("Id", out var idValue);
+                _filterProperties.TryGetValue("Username", out var username);
+                _filterProperties.TryGetValue("FirstName", out var firstName);
+                _filterProperties.TryGetValue("LastName", out var lastName);
+
+                if (int.Parse(idValue) > 0)
+                    query.Where(u => u.Id == int.Parse(idValue));
+                if (!String.IsNullOrEmpty(username))
+                    query.Where(u => u.Username == username);
+                if (!String.IsNullOrEmpty(firstName))
+                    query.Where(u => u.FirstName == firstName);
+                if (!String.IsNullOrEmpty(lastName))
+                    query.Where(u => u.LastName == lastName);
+            }
+        }
+
+        internal class UserUpdatedProperties : UpdatedProperties<UserModel>
+        {
+            public UserUpdatedProperties(Dictionary<string, dynamic> properties) : base(properties)
+            {
+            }
+
+            public override UserModel Set(UserModel user)
+            {
+                _properties.TryGetValue("Username", out var username);
+                _properties.TryGetValue("FirstName", out var firstName);
+                _properties.TryGetValue("LastName", out var lastName);
+                _properties.TryGetValue("Password", out var password);
+
+                user.Username = username ?? user.Username;
+                user.FirstName = firstName ?? user.FirstName;
+                user.LastName = lastName ?? user.LastName;
+                user.Password = password ?? user.Password;
+
+                return user;
+            }
+        }
 
     }
 
--- User/UserRepository.cs
+++ User/UserRepository.cs
@@ -12,104 +12,10 @@ namespace UsersServer.User
     // Repozytorium użytkowników odpowiada za bezpośrednie operacje na bazie danych.
     // Każdej publicznej metodzie odpowiada także metoda prywatna która przekazywana jest do MsSqlDatabaseManager jako delegat a następnie wywoływana z instancją sesji jako argumentem.
     // Dzięki temu logika zapytań pozostaje w repozytorium, a zarządzaniem sesją i tranzakcjami zajmuje się DatabaseManager.
-    class UserRepository : RepositoryTemp
+    class UserRepository : Repository<UserModel>
     {
-        private UserModel _userToAdd;
-        private UserModel _userToUpdate;
-        private UserModel _userToDelete;
-        private SearchCriteria _searchCriteria;
-        private IList<UserModel> _foundUsers;
-
-        public UserRepository(IDatabaseManager databaseManager) : base(databaseManager)
+        public UserRepository(MsSqlDatabaseManager databaseManager) : base(databaseManager)
         {
-        }
-
-        public void Create(UserModel user)
-        {
-            _userToAdd = user;
-            UsersServer.RepositoryCommand cmd = _Create;
-            DatabaseManager.Execute(cmd);
-            Logger.Log("User created.");
-        }
-
-        private void _Create(NHibernate.ISession session)
-        {
-            session.Save(_userToAdd);
-        }
-
-        public IList<UserModel> Read(int id, string firstname, string lastname, string username)
-        {
-            _searchCriteria = new SearchCriteria(id, firstname, lastname, username);
-            UsersServer.RepositoryCommand cmd = _Read;
-            DatabaseManager.Execute(cmd);
-            return _foundUsers;
-        }
-
-        private void _Read(NHibernate.ISession session)
-        {
-            var c = _searchCriteria;
-            var query = session.QueryOver<UserModel>();
-            if (c.Id != 0)
-                query.Where(u => u.Id == c.Id);
-            if (!String.IsNullOrEmpty(c.FirstName))
-                query.Where(u => u.FirstName == c.FirstName);
-            if (!String.IsNullOrEmpty(c.LastName))
-                query.Where(u => u.LastName == c.LastName);
-            if (!String.IsNullOrEmpty(c.Username))
-                query.Where(u => u.Username == c.Username);
-
-            _foundUsers = query.List();
-        }
-
-
-        public void Update(UserModel user, string newFirstName = null, string newLastName = null, string newUsername = null, string newPassword = null)
-        {
-            if (user == null)
-                throw new InvalidOperationException("User does not exist.");
-            user.FirstName = newFirstName ?? user.FirstName;
-            user.LastName = newLastName ?? user.LastName;
-            user.Username = newUsername ?? user.Username;
-            user.Password = newPassword ?? user.Password;
-            
-            _userToUpdate = user;
-            UsersServer.RepositoryCommand cmd = _Update;
-            DatabaseManager.Execute(cmd);
-            Logger.Log("User updated.");
-        }
-
-        private void _Update(NHibernate.ISession session)
-        {
-            session.SaveOrUpdate(_userToUpdate);
-
-        }
-
-        public void Delete(UserModel user)
-        {
-            _userToDelete = user ?? throw new InvalidOperationException("User does not exist.");
-            UsersServer.RepositoryCommand cmd = _Delete;
-            DatabaseManager.Execute(cmd);
-            Logger.Log("User deleted.");
-        }
-
-        private void _Delete(NHibernate.ISession session)
-        {
-            session.Delete(_userToDelete);
-        }
-    }
-
-    internal class SearchCriteria
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Username { get; set; }
-
-        public SearchCriteria(int id, string firstname, string lastname, string username)
-        {
-            Id = id;
-            FirstName = firstname;
-            LastName = lastname;
-            Username = username;
         }
     }
 }
