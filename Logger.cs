44049f46cee318a68572f5abc6c0731a4ed397b5
Rafał <rafal.dardzinski@gmail.com>
2019-03-28 23:23:44 +0100

    testing this

--- Logger.cs
+++ Logger.cs
@@ -3,6 +3,7 @@
 namespace UsersServer
 {
     // Pozwala na logowanie wiadomości. Obecnie po prostu wypisuje wiadomości na konsoli, ale można go w przyszłości rozszerzyć o dodatkowe funkcjonalności.
+    // Testowy feature.
     public class Logger
     {
         public static void Log(string message)
