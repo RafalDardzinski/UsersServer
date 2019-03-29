a926c0cba10e69d225c59e693ad619170593587d
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-03-29 13:53:34 +0100

    Added: MsSqlConnectionString

---
+++ Database/MsSqlConnectionString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UsersServer.Database
+{
+    // Tworzy connection string dla bazy MSSQL.
+    public class MsSqlConnectionString
+    {
+        private readonly string _connectionStringBase = @"Integrated Security=true;"; // dla uproszczenia tylko integrated security
+        private readonly string _serverInstance;
+        private readonly string _databaseName;
+
+        public string Value
+        {
+            get
+            {
+                if (String.IsNullOrWhiteSpace(_serverInstance))
+                    return null;
+                var connectionString = String.Copy(_connectionStringBase);
+                if (!String.IsNullOrWhiteSpace(_serverInstance))
+                    connectionString += $@"Server={_serverInstance};";
+                if (!String.IsNullOrWhiteSpace(_databaseName))
+                    connectionString += $@"Database={_databaseName};";
+                return connectionString;
+            }
+            private set => Value = value;
+        }
+
+        public MsSqlConnectionString(string serverInstance)
+        {
+            _serverInstance = serverInstance;
+        }
+
+        public MsSqlConnectionString(string serverInstance, string databaseName)
+            : this(serverInstance)
+        {
+            _databaseName = databaseName;
+        }
+    }
+}
