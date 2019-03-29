229d967eda646d5c570ec6d26a3c03e23f61e3b7
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-03-29 13:52:25 +0100

    Added: MappingCompiler

---
+++ Database/MappingCompiler.cs
@@ -0,0 +1,21 @@
+﻿
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using UsersServer.Group;
+using UsersServer.User;
+
+namespace UsersServer.Database
+{
+    class MappingCompiler
+    {
+        // Kompiluje modele za pomocą mappera modeli nHibernate (mapping by code).
+        public static HbmMapping CompileModels()
+        {
+            // tutaj wolałbym przekazać listę klas, ale nie wiem jeszcze jak to poprawnie zrobić, dlatego koduję modele na sztywno
+            var modelMapper = new ModelMapper();
+            modelMapper.AddMapping<UserMapping>();
+            modelMapper.AddMapping<GroupMapping>();
+            return modelMapper.CompileMappingForAllExplicitlyAddedEntities();
+        }
+    }
+}
