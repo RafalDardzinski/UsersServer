b3b2698c5462a9610cbd6fa35c7cd0343f278bf4
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-03-29 14:18:42 +0100

    added: UpdatedProperties

---
+++ UpdatedProperties.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace UsersServer
+{
+    public abstract class UpdatedProperties<T> where T : class
+    {
+        protected readonly Dictionary<string, dynamic> _properties;
+
+        protected UpdatedProperties(Dictionary<string, dynamic> properties)
+        {
+            _properties = properties;
+        }
+
+        // Zaktualizuj instancję modelu. Dla każdego modelu musi być zdefiniowana oddzielnie.
+        public abstract T Set(T model);
+    }
+}
