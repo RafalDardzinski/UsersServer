cb327efa11f686e4a8882f597c1c1ef9bbb5c147
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-03-29 14:38:45 +0100

    Added: Group

---
+++ Group/GroupMapping.cs
@@ -0,0 +1,28 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace UsersServer.Group
+{
+    public class GroupMapping : ClassMapping<GroupModel>
+    {
+        public GroupMapping()
+        {
+            Table("Groups");
+            Id(g => g.GroupId, im => im.Generator(Generators.Identity));
+            Property(u => u.Name, nm =>
+            {
+                nm.Length(50);
+                nm.NotNullable(true);
+                nm.Unique(true);
+            });
+
+            Bag(g => g.Users, gm =>
+            {
+                gm.Table("Users2Groups");
+                gm.Key(k => k.Column("UserId"));
+                gm.Cascade(Cascade.None);
+
+            }, m => m.ManyToMany(t => t.Column("GroupId")));
+        }
+    }
+}
---
+++ Group/GroupModel.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+using UsersServer.User;
+
+namespace UsersServer.Group
+{
+    public class GroupModel
+    {
+        public virtual int GroupId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual ICollection<UserModel> Users { get; set; }
+    }
+}
