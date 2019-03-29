8fd72abafcea6da7de05b122206a8f2c7ae65cb9
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-03-29 14:18:27 +0100

    Added: SearchCriteria

---
+++ SearchCriteria.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+using NHibernate;
+
+namespace UsersServer
+{
+    public abstract class SearchCriteria<T> where T : class
+    {
+        protected readonly Dictionary<string, string> _filterProperties;
+
+        protected SearchCriteria(Dictionary<string, string> filterProperties)
+        {
+            _filterProperties = filterProperties;
+        }
+
+        // Modyfikuje zapytanie dodając do niego filtry. Dla każdego modelu musi być zdefiniowana oddzielnie.
+        public abstract void ApplyToQuery(IQueryOver<T, T> query);
+
+    }
+}
