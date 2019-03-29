ec674d107344f6e4f1cef58097c54e17e3b0f9c6
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-03-29 14:46:09 +0100

    added: DataDisplayer module

---
+++ DataDisplayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UsersServer.Group;
+using UsersServer.User;
+
+namespace UsersServer
+{
+    // Klasa "konsumująca" dane i wyświetlająca je na ekranie.
+    class DataDisplayer
+    {
+        public static void Display(IList<UserModel> users)
+        {
+            var displayText = "";
+            foreach (var user in users)
+            {
+                var groups = GetGroupNames(user);
+                displayText += $"\nId: {user.UserId}\nUsername: {user.Username}\nFirstName: {user.FirstName}\nLastName: {user.LastName}\nGroups: {groups}\n";
+            }
+            Logger.Log(displayText);
+
+            // Lokalna metoda agregująca nazwy group do których należy użytkownik i zamieniająca je na ciąg znaków.
+            string GetGroupNames(UserModel user)
+            {
+                var groupNames = user.Groups.Select(g => g.Name);
+                return String.Join(", ", groupNames);
+            }
+        }
+
+        public static void Display(IList<GroupModel> groups)
+        {
+            var displayText = "";
+            foreach (var group in groups)
+            {
+                displayText += $"\nId: {group.GroupId}\nName: {group.Name}\n";
+            }
+            Logger.Log(displayText);
+        }
+    }
+}
