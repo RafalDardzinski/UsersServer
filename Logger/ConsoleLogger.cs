ebbfe00d6ce3451e92003654655d2a1f1813d6b3
Rafał <rafal.dardzinski@gmail.com>
2019-04-08 01:34:31 +0200

    refactored Logger, added interface

---
+++ Logger/ConsoleLogger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UsersServer.Group;
+using UsersServer.User;
+
+namespace UsersServer.Logger
+{
+    public class ConsoleLogger : ILogger
+    {
+        public ConsoleLogger()
+        {
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Log(IList<UserModel> users)
+        {
+            var displayText = "";
+            foreach (var user in users)
+            {
+                var groups = GetGroupNames(user);
+                displayText += $"\nId: {user.UserId}\nUsername: {user.Username}\nFirstName: {user.FirstName}\nLastName: {user.LastName}\nGroups: {groups}\n";
+            }
+            Log(displayText);
+
+            // Lokalna metoda agregująca nazwy group do których należy użytkownik i zamieniająca je na ciąg znaków.
+            string GetGroupNames(UserModel user)
+            {
+                var groupNames = user.Groups.Select(g => g.Name);
+                return String.Join(", ", groupNames);
+            }
+        }
+
+        public void Log(IList<GroupModel> groups)
+        {
+            var displayText = "";
+            foreach (var group in groups)
+            {
+                displayText += $"\nId: {group.GroupId}\nName: {group.Name}\n";
+            }
+            Log(displayText);
+        }
+    }
+}
---
+++ Logger/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UsersServer.Group;
+using UsersServer.User;
+
+namespace UsersServer.Logger
+{
+    public interface ILogger
+    {
+        void Log(string message);
+        void Log(IList<UserModel> users);
+        void Log(IList<GroupModel> groups);
+    }
+}
