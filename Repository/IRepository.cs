3ac07db965fa98832a34d1614e2fe2591abaca93
Rafał <rafal.dardzinski@gmail.com>
2019-04-08 01:38:11 +0200

    Adjusted repo to new logic

--- Repository/IRepository.cs
+++ Repository/IRepository.cs
@@ -3,11 +3,11 @@
 namespace UsersServer.Repository
 {
 	public interface IRepository<T> where T : class
-    {
-	    T FindById(object key);
-        void Create(T modelInstance);
-        IList<T> Read(ISearchCriteria<T> searchCriteria);
-        void Update(T updatedModelInstance);
-        void Delete(T modelInstance);
-    }
+	{
+		T FindById(object key);
+		void Create(T modelInstance);
+		IList<T> Read(ISearchCriteria<T> searchCriteria);
+		void Update(T updatedModelInstance);
+		void Delete(T modelInstance);
+	}
 }
--- Repository/Repository.cs
+++ Repository/Repository.cs
@@ -3,54 +3,45 @@ using System.Collections.Generic;
 
 namespace UsersServer.Repository
 {
-    // Repozytorium odpowiada za wykonywanie podstawowych CRUD-owych operacji na bazie danych.
-    public abstract class Repository<T> : IRepository<T> where T : class
-    {
-        protected readonly NHibernate.ISession Session;
+	// Repozytorium odpowiada za wykonywanie podstawowych CRUD-owych operacji na bazie danych.
+	public abstract class Repository<T> : IRepository<T> where T : class
+	{
+		protected readonly NHibernate.ISession Session;
 
-        protected Repository(NHibernate.ISession session)
-        {
-            Session = session;
-        }
+		protected Repository(NHibernate.ISession session)
+		{
+			Session = session;
+		}
 
-	    public virtual T FindById(object key)
-	    {
-		    return Session.Get<T>(key);
-	    }
+		public virtual T FindById(object key)
+		{
+			return Session.Get<T>(key);
+		}
 
 		// Zapisuje instancję modelu do bazy danych.
 		public virtual void Create(T modelInstance)
-        {
-            Session.Save(modelInstance);
-        }
+		{
+			Session.Save(modelInstance);
+		}
 
-        // Znajdź rekordy i je zwróć.
-        public virtual IList<T> Read(ISearchCriteria<T> searchCriteria)
-        {
-            var query = Session.QueryOver<T>();
-            searchCriteria.ApplyToQuery(query);
-            return query.List();
-        }
+		// Znajdź rekordy i je zwróć.
+		public virtual IList<T> Read(ISearchCriteria<T> searchCriteria)
+		{
+			var query = Session.QueryOver<T>();
+			searchCriteria.ApplyToQuery(query);
+			return query.List();
+		}
 
-        // Zaktualizuj instancję modelu na podstawie przekazanych właściwości.
-        public void Update(T updatedModelInstance)
-        {
-			// review: transaction should be spanned by business logic
-            using (var tx = Session.BeginTransaction())
-            {
-                Session.Update(updatedModelInstance);
-                tx.Commit();
-            }
-        }
+		// Zaktualizuj instancję modelu na podstawie przekazanych właściwości.
+		public void Update(T updatedModelInstance)
+		{
+			Session.Update(updatedModelInstance);
+		}
 
-        // Usuń rekord.
-        public void Delete(T modelInstance)
-        {
-            using (var tx = Session.BeginTransaction())
-            {
-                Session.Delete(modelInstance);
-                tx.Commit();
-            }
-        }
-    }
+		// Usuń rekord.
+		public void Delete(T modelInstance)
+		{
+			Session.Delete(modelInstance);
+		}
+	}
 }
