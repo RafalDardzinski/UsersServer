5eac82782abce885036077c82f2896b6c0461634
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-04-26 10:58:02 +0200

    added: bluebird polyfill

--- Web-Api/App_Start/WebApiConfig.cs
+++ Web-Api/App_Start/WebApiConfig.cs
@@ -1,13 +1,10 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Web_Api
 {
-    public static class WebApiConfig
+  public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
